3d5f8efc9f583bace561419c55aaf9ae95b842a8
Ramen Tabetarou <kobeengineer1225@gmail.com>
2021-02-08 21:57:06 +0900

    redux counter app
    Signed-off-by: Ramen Tabetarou <kobeengineer1225@gmail.com>

--- redux-sample/src/components/Counter/container.jsx
+++ redux-sample/src/components/Counter/container.jsx
@@ -1,3 +1,26 @@
 import Counter from "./presentation";
+import { connect } from "react-redux";
+import { increment, decrement } from "../../redux/count/actions";
 
-export default Counter;
+//storeからどのstateを引っ張ってくるか
+const mapStateProps = ({ count }) => ({ count });
+
+//上は以下と同義
+//const mapStateProps = state => {
+//    return { count: state.count };
+//};
+
+//どんなdispatcherをpropsに渡すか
+const mapDispatchProps = dispatch => ({
+    increment: count => {
+        dispatch(increment(count));
+    },
+    decrement: count => {
+        dispatch(decrement(count));
+    }
+});
+
+export default connect(
+    mapStateProps,
+    mapDispatchProps
+)(Counter);
